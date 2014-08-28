96bc0351ce1ed80a43acd0d3bed4de94f67fd507
lovsch146 <lovsch146@E29606.edu.linkoping.se>
2014-08-28 14:54:52 +0200

    första

---
+++ variabel1.php
@@ -0,0 +1,54 @@
+<meta charset="UTF-8">
+<?php
+$ko = 2;
+$katt = 4;
+
+echo $katt.$ko."<br>"."<br>";
+$hast = $katt+$ko;
+
+echo $hast."<br>"."<br>";
+
+$kossa = "bacon";
+$katten = "os";
+
+echo "Om man slaktar en gris blir det "."$kossa" ." och då luktar det "."$kossa"."$katten";
+
+        
+
+
+
+
+$a = 2;
+$b = 3;
+
+if($a<5){
+    echo "<br>"."<br>"."RÄTT";
+}
+
+else{
+    echo "FEL";
+}
+
+
+
+$c = 4;
+
+switch ($c) {
+    case 1:
+        
+        echo "<br>"."<br>"."HEJ";
+
+
+        break;
+    case 4:
+        
+        echo "<br>"."<br>"."HEJDÅ";
+        
+        break;
+
+    default:
+        break;
+}
+        
+?>
+
